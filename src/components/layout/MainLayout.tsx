3beae9f0a89565443d4146d5b18df4ea9f1c8ae9
ThomasHSON <hson_thomas@kutech.tw>
2026-05-13 16:29:56 +0800

    Updated MainLayout.tsx

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -14,7 +14,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children, title }) => {
   return (
     <div className="min-h-screen flex flex-col bg-white">
       <main className="flex-1 p-4 md:p-6 lg:p-8 pb-8">
-        <div className="max-w-screen-xl mx-auto">
+        <div className="mx-auto">
           <Header title={title} />
           {children}
         </div>
